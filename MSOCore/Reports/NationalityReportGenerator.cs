e1301c3b87ac795d83d862c864094a0bfcb30db1
juliahayward <JHayward@Eleanor-LP.cp.onalytica.com>
2015-06-21 17:31:17 +0100

    Nationality report generator pulls logic out of WPF

---
+++ MSOCore/Reports/NationalityReportGenerator.cs
@@ -0,0 +1,45 @@
+﻿using MSOOrganiser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSOCore.Reports
+{
+    public class NationalityReportGenerator
+    {
+        public class NationalityVm
+        {
+            public string Nationality { get; set; }
+            public string Males { get; set; }
+            public string Females { get; set; }
+            public string Total { get; set; }
+        }
+
+        public IEnumerable<NationalityVm> GetItems()
+        {
+            var context = new DataEntities();
+            var contIds = context.Entrants.Where(x => x.Year == 2014) // TODO current olympiad
+                .Select(x => x.Mind_Sport_ID).Distinct().ToList();
+            var conts = context.Contestants.Where(x => contIds.Contains(x.Mind_Sport_ID))
+                .ToList();
+
+            var totals = conts.GroupBy(x => x.Nationality ?? "Other")
+                .ToDictionary(x => x.Key, x => x.Count());
+            var males = conts.Where(x => !x.Male.HasValue || x.Male.Value).GroupBy(x => x.Nationality ?? "Other")
+                .ToDictionary(x => x.Key, x => x.Count());
+            var females = conts.Where(x => x.Male.HasValue && !x.Male.Value).GroupBy(x => x.Nationality ?? "Other")
+                .ToDictionary(x => x.Key, x => x.Count());
+            foreach (var key in totals.Keys.OrderBy(x => x))
+                yield return new NationalityVm()
+                {
+                    Nationality = key,
+                    Males = males.ContainsKey(key) ? males[key].ToString() : "",
+                    Females = females.ContainsKey(key) ? females[key].ToString() : "",
+                    Total = totals[key].ToString()
+                };
+
+        }
+    }
+}
--- MSOOrganiser/NationalityReport.xaml.cs
+++ MSOOrganiser/NationalityReport.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MSOCore.Reports;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
@@ -21,14 +22,6 @@ namespace MSOOrganiser
     /// </summary>
     public partial class NationalityReport : UserControl
     {
-        public class NationalityVm 
-        { 
-            public string Nationality { get; set; } 
-            public string Males { get; set; }
-            public string Females { get; set; }
-            public string Total { get; set; }
-        }
-
         public NationalityReport()
         {
             InitializeComponent();
@@ -37,33 +30,15 @@ namespace MSOOrganiser
             dataGrid.DataContext = Items;
         }
 
-        ObservableCollection<NationalityVm> Items { get; set; }
+        ObservableCollection<NationalityReportGenerator.NationalityVm> Items { get; set; }
 
         public void Populate()
         {
-            Items = new ObservableCollection<NationalityVm>();
-            
-            var context = new DataEntities();
-            var contIds = context.Entrants.Where(x => x.Year == 2014) // TODO current olympiad
-                .Select(x => x.Mind_Sport_ID).Distinct().ToList();
-            var conts = context.Contestants.Where(x => contIds.Contains(x.Mind_Sport_ID))
-                .ToList();
+            var generator = new NationalityReportGenerator();
 
-            var totals = conts.GroupBy(x => x.Nationality ?? "Other")
-                .ToDictionary(x => x.Key, x => x.Count());
-            var males = conts.Where(x => !x.Male.HasValue || x.Male.Value).GroupBy(x => x.Nationality ?? "Other")
-                .ToDictionary(x => x.Key, x => x.Count());
-            var females = conts.Where(x => x.Male.HasValue && !x.Male.Value).GroupBy(x => x.Nationality ?? "Other")
-                .ToDictionary(x => x.Key, x => x.Count());
-            foreach (var key in totals.Keys.OrderBy(x => x))
-                Items.Add(new NationalityVm() 
-                    { 
-                        Nationality = key, 
-                        Males = males.ContainsKey(key) ? males[key].ToString() : "",
-                        Females = females.ContainsKey(key) ? females[key].ToString() : "",
-                        Total = totals[key].ToString()
-                    });
-
+            Items = new ObservableCollection<NationalityReportGenerator.NationalityVm>();
+            foreach (var item in generator.GetItems())
+                Items.Add(item);
         }
 
         private void print_Click(object sender, RoutedEventArgs e)
