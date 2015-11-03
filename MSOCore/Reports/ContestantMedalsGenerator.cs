58b2937d7e9dd230af52e20a840c2de4d8c74889
juliahayward <JHayward@Eleanor-LP.cp.onalytica.com>
2015-11-03 21:05:39 +0000

    Show titles on individual report

--- MSOCore/Reports/ContestantMedalsGenerator.cs
+++ MSOCore/Reports/ContestantMedalsGenerator.cs
@@ -10,8 +10,33 @@ namespace MSOCore.Reports
     {
         public class ContestantVm
         {
+            public class MedalVm
+            {
+                public int Year { get; set; }
+                public string Name { get; set; }
+                public string Medal { get; set; }
+                public string Code { get; set; }
+            }
+
             public int Id { get; set; }
             public string Name { get; set; }
+            public string Nationality { get; set; }
+            public string Flag
+            {
+                get
+                {
+                    return string.Format("http://www.boardability.com/images/flags/{0}.jpg", Nationality)
+                        .Replace(" ", "_").ToLower();
+                }
+            }
+
+            public IEnumerable<MedalVm> Medals { get; set; }
+            public int Golds { get { return Medals.Count(x => x.Medal.StartsWith("Gold")); } }
+            public int Silvers { get { return Medals.Count(x => x.Medal.StartsWith("Silver")); } }
+            public int Bronzes { get { return Medals.Count(x => x.Medal.StartsWith("Bronze")); } }
+            public IEnumerable<string> Grandmasters { get; set; }
+            public IEnumerable<string> InternationalMasters { get; set; }
+            public IEnumerable<string> CandidateMasters { get; set; }
         }
 
         public ContestantVm GetModel(int id)
@@ -20,10 +45,41 @@ namespace MSOCore.Reports
 
             var contestant = context.Contestants.First(x => x.Mind_Sport_ID == id);
 
+            var medalEvents = context.Entrants.Where(x => x.Mind_Sport_ID == id && x.Medal != null)
+                .Join(context.Events, 
+                x => new { code = x.Game_Code, year = x.OlympiadId }, 
+                e => new { code = e.Code, year = e.OlympiadId }, (x, e) => new { x, e })
+                .Select(xe => new ContestantVm.MedalVm { Year = xe.x.Year.Value, Code = xe.x.Game_Code, Name = xe.e.Mind_Sport, Medal = xe.x.Medal})
+                .OrderBy(x => x.Year).ToList();
+
+            var gmCodes = new List<string>();
+            var imCodes = new List<string>();
+            var cmCodes = new List<string>();
+            foreach (var code in medalEvents.Select(x => x.Code).Distinct())
+            {
+                var golds = medalEvents.Count(x => x.Code == code && x.Medal == "Gold");
+                var silvers = medalEvents.Count(x => x.Code == code && x.Medal == "Silver");
+                var bronzes = medalEvents.Count(x => x.Code == code && x.Medal == "Bronze");
+                if (golds >= 2 || (golds == 1 && silvers >= 2))
+                    gmCodes.Add(code);
+                else if ((golds == 1 && silvers + bronzes > 0)
+                    || silvers >= 2
+                    || (silvers == 1 && bronzes >= 2))
+                    imCodes.Add(code);
+                else if ((silvers == 1 && bronzes > 0)
+                    || bronzes >= 2)
+                    cmCodes.Add(code);
+            }
+
             var model = new ContestantVm
             {
                 Id = id,
-                Name = string.Format("{0} {1}", contestant.Firstname, contestant.Lastname)
+                Name = string.Format("{0} {1}", contestant.Firstname, contestant.Lastname),
+                Nationality = contestant.Nationality,
+                Medals = medalEvents,
+                Grandmasters = gmCodes,
+                InternationalMasters = imCodes,
+                CandidateMasters = cmCodes
             };
 
             return model;
