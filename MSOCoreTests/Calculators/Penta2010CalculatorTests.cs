9f969094795a5c4fb8d9a33a2e553f4811fb5975
juliahayward <JHayward@Eleanor-LP.cp.onalytica.com>
2015-12-24 19:09:39 +0000

    More tests

---
+++ MSOCoreTests/Calculators/Penta2010CalculatorTests.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSOCore.Calculators;
+
+namespace MSOCoreTests.Calculators
+{
+    [TestClass]
+    public class Penta2010CalculatorTests
+    {
+        [TestMethod]
+        public void CalculatorThrowsOnMissingRank()
+        {
+            var data = new[] { new TestCalc { Rank = 0, PentaScore = "", Absent = false, Id = 1 } };
+
+            var calc = new Penta2010Calculator();
+            try
+            {
+                calc.Calculate(1, data);
+                Assert.Fail("Should throw");
+            }
+            catch (ArgumentException) { }
+            catch (Exception) { Assert.Fail("Threw wrong exception"); }
+        }
+
+        [TestMethod]
+        public void CalculatorGetsRightAnswerInSimpleCase()
+        {
+            var data = new[] { 
+                new TestCalc { Rank = 1, PentaScore = "", Absent = false, Id = 1 },
+                new TestCalc { Rank = 3, PentaScore = "", Absent = false, Id = 2 },
+                new TestCalc { Rank = 5, PentaScore = "", Absent = false, Id = 3 },
+                new TestCalc { Rank = 0, PentaScore = "", Absent = true, Id = 13 },
+                new TestCalc { Rank = 4, PentaScore = "", Absent = false, Id = 4 },
+                new TestCalc { Rank = 2, PentaScore = "", Absent = false, Id = 5 },
+            };
+
+            var calc = new Penta2010Calculator();
+            calc.Calculate(1, data);
+
+            Assert.AreEqual("83.3333333333333", data[0].PentaScore);
+            Assert.AreEqual("41.6666666666667", data[1].PentaScore);
+            Assert.AreEqual("0", data[2].PentaScore);
+            Assert.AreEqual("", data[3].PentaScore);
+            Assert.AreEqual("20.8333333333333", data[4].PentaScore);
+            Assert.AreEqual("62.5", data[5].PentaScore);
+        }
+
+        [TestMethod]
+        public void CalculatorGetsRightAnswerWhenTie()
+        {
+            var data = new[] { 
+                new TestCalc { Rank = 1, PentaScore = "", Absent = false, Id = 1 },
+                new TestCalc { Rank = 3, PentaScore = "", Absent = false, Id = 2 },
+                new TestCalc { Rank = 5, PentaScore = "", Absent = false, Id = 3 },
+                new TestCalc { Rank = 0, PentaScore = "", Absent = true, Id = 13 },
+                new TestCalc { Rank = 4, PentaScore = "", Absent = false, Id = 4 },
+                new TestCalc { Rank = 1, PentaScore = "", Absent = false, Id = 5 },
+            };
+
+            var calc = new Penta2010Calculator();
+            calc.Calculate(1, data);
+
+            Assert.AreEqual("72.9166666666667", data[0].PentaScore);
+            Assert.AreEqual("41.6666666666667", data[1].PentaScore);
+            Assert.AreEqual("0", data[2].PentaScore);
+            Assert.AreEqual("", data[3].PentaScore);
+            Assert.AreEqual("20.8333333333333", data[4].PentaScore);
+            Assert.AreEqual("72.9166666666667", data[5].PentaScore);
+        }
+
+        [TestMethod]
+        public void CalculatorGetsRightAnswerWhenTwoPerTeam()
+        {
+            var data = new[] { 
+                new TestCalc { Rank = 1, PentaScore = "", Absent = false, Id = 1 },
+                new TestCalc { Rank = 3, PentaScore = "", Absent = false, Id = 2 },
+                new TestCalc { Rank = 5, PentaScore = "", Absent = false, Id = 3 },
+                new TestCalc { Rank = 0, PentaScore = "", Absent = true, Id = 13 },
+                new TestCalc { Rank = 4, PentaScore = "", Absent = false, Id = 4 },
+                new TestCalc { Rank = 2, PentaScore = "", Absent = false, Id = 5 },
+                new TestCalc { Rank = 1, PentaScore = "", Absent = false, Id = 101 },
+                new TestCalc { Rank = 3, PentaScore = "", Absent = false, Id = 102 },
+                new TestCalc { Rank = 5, PentaScore = "", Absent = false, Id = 103 },
+                new TestCalc { Rank = 0, PentaScore = "", Absent = true, Id = 113 },
+                new TestCalc { Rank = 4, PentaScore = "", Absent = false, Id = 104 },
+                new TestCalc { Rank = 2, PentaScore = "", Absent = false, Id = 105 },
+            };
+
+            var calc = new Penta2010Calculator();
+            calc.Calculate(2, data);
+
+            Assert.AreEqual("83.3333333333333", data[0].PentaScore);
+            Assert.AreEqual("41.6666666666667", data[1].PentaScore);
+            Assert.AreEqual("0", data[2].PentaScore);
+            Assert.AreEqual("", data[3].PentaScore);
+            Assert.AreEqual("20.8333333333333", data[4].PentaScore);
+            Assert.AreEqual("62.5", data[5].PentaScore);
+        }
+    }
+}
---
+++ MSOCoreTests/Calculators/RankCheckerTests.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSOCore.Calculators;
+
+namespace MSOCoreTests.Calculators
+{
+    [TestClass]
+    public class RankCheckerTests
+    {
+        [TestMethod]
+        public void CheckerGetsRightAnswerInSimpleCase()
+        {
+            var data = new[] { 
+                new TestCalc { Rank = 1, PentaScore = "", Absent = false, Id = 1 },
+                new TestCalc { Rank = 3, PentaScore = "", Absent = false, Id = 2 },
+                new TestCalc { Rank = 5, PentaScore = "", Absent = false, Id = 3 },
+                new TestCalc { Rank = 0, PentaScore = "", Absent = true, Id = 13 },
+                new TestCalc { Rank = 4, PentaScore = "", Absent = false, Id = 4 },
+                new TestCalc { Rank = 2, PentaScore = "", Absent = false, Id = 5 },
+            };
+
+            var c = new RankChecker();
+            c.Check(1, data);
+        }
+
+        [TestMethod]
+        public void CheckerCatchesZeroRank()
+        {
+            var data = new[] { 
+                new TestCalc { Rank = 1, PentaScore = "", Absent = false, Id = 1 },
+                new TestCalc { Rank = 3, PentaScore = "", Absent = false, Id = 2 },
+                new TestCalc { Rank = 5, PentaScore = "", Absent = false, Id = 3 },
+                new TestCalc { Rank = 0, PentaScore = "", Absent = false, Id = 13 },
+                new TestCalc { Rank = 4, PentaScore = "", Absent = false, Id = 4 },
+                new TestCalc { Rank = 2, PentaScore = "", Absent = false, Id = 5 },
+            };
+
+            var c = new RankChecker();
+            try 
+            { 
+                c.Check(1, data);
+                Assert.Fail("Missed zero rank");
+            }
+            catch {}
+        }
+
+        [TestMethod]
+        public void CheckerCatchesMissingRank()
+        {
+            var data = new[] { 
+                new TestCalc { Rank = 1, PentaScore = "", Absent = false, Id = 1 },
+                new TestCalc { Rank = 3, PentaScore = "", Absent = false, Id = 2 },
+                new TestCalc { Rank = 5, PentaScore = "", Absent = false, Id = 3 },
+                new TestCalc { Rank = 7, PentaScore = "", Absent = false, Id = 13 },
+                new TestCalc { Rank = 4, PentaScore = "", Absent = false, Id = 4 },
+                new TestCalc { Rank = 2, PentaScore = "", Absent = false, Id = 5 },
+            };
+
+            var c = new RankChecker();
+            try
+            {
+                c.Check(1, data);
+                Assert.Fail("Missed zero rank");
+            }
+            catch { }
+        }
+
+        [TestMethod]
+        public void CheckerCatchesDuplicateRank()
+        {
+            var data = new[] { 
+                new TestCalc { Rank = 1, PentaScore = "", Absent = false, Id = 1 },
+                new TestCalc { Rank = 3, PentaScore = "", Absent = false, Id = 2 },
+                new TestCalc { Rank = 5, PentaScore = "", Absent = false, Id = 3 },
+                new TestCalc { Rank = 3, PentaScore = "", Absent = false, Id = 13 },
+                new TestCalc { Rank = 4, PentaScore = "", Absent = false, Id = 4 },
+                new TestCalc { Rank = 2, PentaScore = "", Absent = false, Id = 5 },
+            };
+
+            var c = new RankChecker();
+            try
+            {
+                c.Check(1, data);
+                Assert.Fail("Missed duplicate rank");
+            }
+            catch { }
+        }
+
+        [TestMethod]
+        public void CheckerHandlesDuplicateRankWithGapAfter()
+        {
+            var data = new[] { 
+                new TestCalc { Rank = 1, PentaScore = "", Absent = false, Id = 1 },
+                new TestCalc { Rank = 3, PentaScore = "", Absent = false, Id = 2 },
+                new TestCalc { Rank = 5, PentaScore = "", Absent = false, Id = 3 },
+                new TestCalc { Rank = 3, PentaScore = "", Absent = false, Id = 13 },
+                new TestCalc { Rank = 6, PentaScore = "", Absent = false, Id = 4 },
+                new TestCalc { Rank = 2, PentaScore = "", Absent = false, Id = 5 },
+            };
+
+            var c = new RankChecker();
+            c.Check(1, data);
+        }
+
+        [TestMethod]
+        public void CheckerHandlesTeams()
+        {
+            var data = new[] { 
+                new TestCalc { Rank = 1, PentaScore = "", Absent = false, Id = 1 },
+                new TestCalc { Rank = 3, PentaScore = "", Absent = false, Id = 2 },
+                new TestCalc { Rank = 2, PentaScore = "", Absent = false, Id = 3 },
+                new TestCalc { Rank = 3, PentaScore = "", Absent = false, Id = 13 },
+                new TestCalc { Rank = 1, PentaScore = "", Absent = false, Id = 4 },
+                new TestCalc { Rank = 2, PentaScore = "", Absent = false, Id = 5 },
+            };
+
+            var c = new RankChecker();
+            c.Check(2, data);
+        }
+
+        [TestMethod]
+        public void CheckerCatchesMissingInTeams()
+        {
+            var data = new[] { 
+                new TestCalc { Rank = 1, PentaScore = "", Absent = false, Id = 1 },
+                new TestCalc { Rank = 3, PentaScore = "", Absent = false, Id = 2 },
+                new TestCalc { Rank = 2, PentaScore = "", Absent = false, Id = 3 },
+                new TestCalc { Rank = 4, PentaScore = "", Absent = false, Id = 13 },
+                new TestCalc { Rank = 1, PentaScore = "", Absent = false, Id = 4 },
+                new TestCalc { Rank = 2, PentaScore = "", Absent = false, Id = 5 },
+            };
+
+            var c = new RankChecker();
+            try
+            {
+                c.Check(2, data);
+                Assert.Fail("Missed duplicate");
+            }
+            catch { }
+        }
+    }
+}
