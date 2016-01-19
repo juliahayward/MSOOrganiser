650c289aa005eef47bf24b5ac2f303053f31513d
juliahayward <JHayward@Eleanor-LP.cp.onalytica.com>
2016-01-19 22:08:56 +0000

    SpinnyCursor pattern

---
+++ MSOOrganiser/UIUtilities/SpinnyCursor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace MSOOrganiser.UIUtilities
+{
+    public class SpinnyCursor : IDisposable
+    {
+        public SpinnyCursor()
+        {
+            Mouse.OverrideCursor = Cursors.Wait;
+        }
+
+        public void Dispose()
+        {
+            Mouse.OverrideCursor = Cursors.Arrow;
+        }
+    }
+}
