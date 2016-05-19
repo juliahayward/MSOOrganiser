4141c9cd05166c0046bba69868f6751bfd59ac27
juliahayward <JHayward@Eleanor-LP.cp.onalytica.com>
2016-05-19 22:18:50 +0100

    Events entrants on the web

---
+++ MSOCore/Reports/EventEntrantsGenerator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSOCore.Reports
+{
+    public class EventEntrantsGenerator
+    {
+        public class EventEntrantsVm
+        {
+            public int EventYear { get; set; }
+            public string EventName { get; set; }
+            public IEnumerable<EntrantVm> Entrants { get; set; }
+
+            public class EntrantVm
+            {
+                public string Name { get; set; }
+            }
+        }
+
+        public EventEntrantsVm GetModel(int? year, string gameCode)
+        {
+            var vm = new EventEntrantsVm();
+            var context = DataEntitiesProvider.Provide();
+
+            var olympiad = (year.HasValue)
+                ? context.Olympiad_Infoes.First(x => x.YearOf == year.Value)
+                : context.Olympiad_Infoes.OrderByDescending(x => x.YearOf).First();
+
+            vm.EventYear = olympiad.YearOf.Value;
+
+            var evt = context.Events.First(x => x.OlympiadId == olympiad.Id && x.Code == gameCode);
+            vm.EventName = evt.Mind_Sport;
+            vm.Entrants = evt.Entrants
+                .Select(x => new EventEntrantsVm.EntrantVm() { Name = x.Name.FullName() })
+                .OrderBy(x => x.Name);
+            return vm;
+        }
+    }
+}
---
+++ MSOWeb/Controllers/EventController.cs
@@ -0,0 +1,25 @@
+﻿using MSOCore.Reports;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MSOWeb.Controllers
+{
+    public class EventController : Controller
+    {
+        //
+        // GET: /Event/
+
+        public ActionResult Entrants(int year, string eventCode)
+        {
+            var generator = new EventEntrantsGenerator();
+
+            var model = generator.GetModel(year, eventCode);
+
+            return View(model);
+        }
+
+    }
+}
