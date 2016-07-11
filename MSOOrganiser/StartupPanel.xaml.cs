1a24e0510dcc939f4388df0f837640e240f9c446
juliahayward <JHayward@Eleanor-LP.cp.onalytica.com>
2016-07-11 19:11:53 +0100

    Suppress script errors on browser

--- MSOOrganiser/StartupPanel.xaml.cs
+++ MSOOrganiser/StartupPanel.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +24,22 @@ namespace MSOOrganiser
         public StartupPanel()
         {
             InitializeComponent();
+
+            HideScriptErrors(this.Browser, true);
+        }
+
+        public void HideScriptErrors(WebBrowser wb, bool hide)
+        {
+        http://stackoverflow.com/questions/1298255/how-do-i-suppress-script-errors-when-using-the-wpf-webbrowser-control
+            var fiComWebBrowser = typeof(WebBrowser).GetField("_axIWebBrowser2", BindingFlags.Instance | BindingFlags.NonPublic);
+            if (fiComWebBrowser == null) return;
+            var objComWebBrowser = fiComWebBrowser.GetValue(wb);
+            if (objComWebBrowser == null)
+            {
+                wb.Loaded += (o, s) => HideScriptErrors(wb, hide); //In case we are to early
+                return;
+            }
+            objComWebBrowser.GetType().InvokeMember("Silent", BindingFlags.SetProperty, null, objComWebBrowser, new object[] { hide });
         }
     }
 }
