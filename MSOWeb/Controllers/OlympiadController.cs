a520dc671a1b2c964989d941901f972131c89d18
Julia Hayward <julia.hayward@red-gate.com>
2019-09-23 13:14:48 +0100

    Start prototype Olympiad editing page (pattern for the others)

---
+++ MSOWeb/Controllers/OlympiadController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MSOWeb.Controllers
+{
+    public class OlympiadController : Controller
+    {
+        //
+        // GET: /Olympiad/
+
+        public ActionResult Index()
+        {
+            // returns a list of them
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Olympiad(int id)
+        {
+            // get the page
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Olympiad(/* form*/)
+        {
+            // post to update
+            return new EmptyResult();
+        }
+
+    }
+}
