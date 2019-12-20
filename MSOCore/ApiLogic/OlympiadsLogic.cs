1e0579a699ac071ad18498c9d0c43f094d2b5073
Julia Hayward <julia.hayward@red-gate.com>
2019-12-20 16:47:53 +0000

    Add Event page

--- MSOCore/ApiLogic/OlympiadsLogic.cs
+++ MSOCore/ApiLogic/OlympiadsLogic.cs
@@ -27,8 +27,34 @@ namespace MSOCore.ApiLogic
 
             public class EventVm
             {
+                public int EventId { get; set; }
                 public string Code { get; set; }
                 public string Name { get; set; }
+            }
+        }
+
+        public class EventVm
+        {
+            public int EventId { get; set; }
+            public string Code { get; set; }
+            public string Name { get; set; }
+
+            public IEnumerable<EntrantVm> Entrants { get; set; }
+
+            public class EntrantVm
+            {
+                public int EntrantId { get; set; }
+                public string FirstName { get; set; }
+                public string LastName { get; set; }
+                public string Medal { get; set; }
+                public string JuniorMedal { get; set; }
+                public int? Rank { get; set; }
+                public string Score { get; set; }
+                public bool Absent { get; set; }
+                public string Tiebreak { get; set; }
+                public double Pentamind { get; set; }
+
+                public string FullName() { return FirstName + " " + LastName.ToUpper(); }
             }
         }
 
@@ -58,8 +84,34 @@ namespace MSOCore.ApiLogic
                 Events = x.Events.Select(e => new OlympiadVm.EventVm()
                 {
                     Code = e.Code,
-                    Name = e.Mind_Sport
+                    Name = e.Mind_Sport,
+                    EventId = e.EIN
                 }).OrderBy(e => e.Code)
+            }).First();
+        }
+
+        public EventVm GetEvent(int id)
+        {
+            var context = DataEntitiesProvider.Provide();
+
+            return context.Events.Where(x => x.EIN == id).Select(e => new EventVm()
+            {
+                EventId = id,
+                Code = e.Code,
+                Name = e.Mind_Sport,
+                Entrants = e.Entrants.Select(en => new EventVm.EntrantVm()
+                {
+                    EntrantId = en.Mind_Sport_ID.Value,
+                    FirstName = en.Name.Firstname,
+                    LastName = en.Name.Lastname,
+                    Medal = en.Medal,
+                    JuniorMedal = en.JuniorMedal,
+                    Rank = en.Rank,
+                    Score = en.Score,
+                    Absent = en.Absent,
+                    Tiebreak = en.Tie_break,
+                    Pentamind = en.Penta_Score.Value
+                }).OrderBy(x => x.Rank).ThenBy(x => x.LastName).ThenBy(x => x.FirstName)
             }).First();
         }
     }
--- MSOWeb/Controllers/OlympiadController.cs
+++ MSOWeb/Controllers/OlympiadController.cs
@@ -34,5 +34,13 @@ namespace MSOWeb.Controllers
             return new EmptyResult();
         }
 
+        [HttpGet]
+        public ActionResult Event(int id)
+        {
+            var logic = new OlympiadsLogic();
+            var model = logic.GetEvent(id);
+
+            return View(model);
+        }
     }
 }
